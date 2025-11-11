b3a1e45d77d3c5e749c2cd254f40dbaaefccc750
Danil <overlordforever@yandex.ru>
2025-11-11 22:42:00 +0300

    feat: добавлен компонент ROC-кривой для анализа классификации
    - Создан RocChartWidget на основе fl_chart
    - Добавлена визуализация кривой и диагонали случайного классификатора
    - Реализованы tooltips с FPR, TPR и порогами
    - Интеграция с FraudAnalysisModel
    
    refactor: добавлено событие загрузки данных об анализе мошенничества с банковских карт с сервера

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_gl_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterGlMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterGlMacosPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
